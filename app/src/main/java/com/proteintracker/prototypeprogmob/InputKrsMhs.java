96b17ba3b7b2b8a5b807fd9baf1742804eb45313
RickKonzern <rickkonzern@gmail.com>
2019-10-23 22:53:50 +0700

    Initial commit

---
+++ app/src/main/java/com/proteintracker/prototypeprogmob/InputKrsMhs.java
@@ -0,0 +1,14 @@
+package com.proteintracker.prototypeprogmob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InputKrsMhs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_input_krs_mhs);
+    }
+}
