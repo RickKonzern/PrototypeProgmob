216c67f181cdfb90315047b9490e432ebeeb74c9
RickKonzern <rickkonzern@gmail.com>
2019-10-24 20:24:37 +0700

    Prototype

---
+++ app/src/main/java/com/proteintracker/prototypeprogmob/LihatDataMhs.java
@@ -0,0 +1,14 @@
+package com.proteintracker.prototypeprogmob;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LihatDataMhs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_data_mhs);
+    }
+}
